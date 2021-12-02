fac8dfde511fb6058abb755db309653701443e96
rudra-9197 <91685979+rudra-9197@users.noreply.github.com>
2021-12-02 13:46:15 +0530

    outputprocess

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var buttonTranslate = document.querySelector("#button_translte");
 var txtInput =document.querySelector("#text_input");
+var outputbox = document.querySelector("#output");
+
 console.log(txtInput);
 
 
 function clickEventHandler(){
-    console.log("clicked");
-    console.log("input",txtInput.value);
-}
+    outputbox.innerText = " akskasaksks" + txtInput.value
+};
 
 
-buttonTranslate.addEventListener("click",  clickEventHandler())
+buttonTranslate.addEventListener("click",  clickEventHandler)
 

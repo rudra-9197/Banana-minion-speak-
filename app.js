bd87940af7689813d1f80a515ef4118834829746
rudra-9197 <91685979+rudra-9197@users.noreply.github.com>
2021-12-02 18:02:41 +0530

    update app.js

--- app.js
+++ app.js
@@ -2,11 +2,27 @@ var buttonTranslate = document.querySelector("#button_translte");
 var txtInput =document.querySelector("#text_input");
 var outputbox = document.querySelector("#output");
 
-console.log(txtInput);
+var serverURL ="https://api.funtranslations.com/translate/minion.json"
 
+function getTranslationURL(text){
+    return serverURL + "?" +"text="+ text
+}
 
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong with the server!try again after some time")
+
+}
 function clickEventHandler(){
-    outputbox.innerText = " akskasaksks" + txtInput.value
+    var inputText = txtInput.Value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response=>response.json())
+
+    
+    .catch(errorHandler)
+
+    
 };
 
 
